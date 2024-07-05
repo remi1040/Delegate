a4dcca65cd20c80eedeeeb4ca6745f9ab3c236e8
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-07-05 10:05:53 +0200

    Add files via upload

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        ShowMoney show = Euro;
+        Console.WriteLine(show(2));
+        show = Dollar;
+        Console.WriteLine(show(2));
+    }
+
+    public delegate string ShowMoney(int x);
+
+    public static string Dollar(int x)
+    {
+        return $"{x} $" ;
+    }
+
+    public static string Euro(int x)
+    {
+        return $"{x} €";
+    }
+}
